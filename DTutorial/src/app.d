bc4330a7886c38b03e4c2a1156827b62c6006440
Evangelos Angelou <evangelos.angelou@upstreamsystems.com>
2019-07-03 07:57:29 +0300

    dfmt

--- DTutorial/src/app.d
+++ DTutorial/src/app.d
@@ -5,48 +5,58 @@ static string hardcoded = "0.0.0";
 
 int main(string[] args)
 {
-//    Version tc = new dversion.core.Version(1, 2, 3);
-//    tc.printVersionJSON();
-//
-//    auto newVersion = parseVersion("1.12");
-//    newVersion.printVersionJSON();
+    //    Version tc = new dversion.core.Version(1, 2, 3);
+    //    tc.printVersionJSON();
+    //
+    //    auto newVersion = parseVersion("1.12");
+    //    newVersion.printVersionJSON();
 
     uint major, minor, patch;
     string versionString;
     bool json;
-    auto helpInformation = getopt(args, "m|major", "Version major number.", &major, // numeric
+    auto helpInformation = getopt(args, "m|major", "Version major number.",
+            &major, // numeric
             "n|minor", "Version minor number.", &minor, // numeric
-            "p|patch", "Version patch number.", &patch, // numeric
-            "v|version", "Version as parsable  ('.' separated) version string. Ommited numbers will be considered 0.
-             If json is enabled, a json representation is assumed (e.g. {\"version\": \"1.2.3\"}).", &versionString, // string 
+            "p|patch",
+            "Version patch number.",
+            &patch, // numeric
+            "v|version",
+            "Version as parsable  ('.' separated) version string. Ommited numbers will be considered 0.
+             If json is enabled, a json representation is assumed (e.g. {\"version\": \"1.2.3\"}).",
+            &versionString, // string 
             "j|json", "Enable JSON version parsing and output.", &json // bool 
             ); // enum
-            
+
     if (helpInformation.helpWanted)
     {
         defaultGetoptPrinter("Some information about the program.", helpInformation.options);
         return 0;
     }
 
-	Version flagVersion;
+    Version flagVersion;
     if (versionString)
     {
-    	
-    	if (json) {
-	        flagVersion = parseVersionJSON(versionString);
-	        flagVersion.printVersionJSON();
-    	} else {
-    		flagVersion = parseVersion(versionString);
-	        flagVersion.printVersion();
-    	}
+        if (json)
+        {
+            flagVersion = parseVersionJSON(versionString);
+            flagVersion.printVersionJSON();
+        }
+        else
+        {
+            flagVersion = parseVersion(versionString);
+            flagVersion.printVersion();
+        }
     }
     else
     {
         flagVersion = new dversion.core.Version(major, minor, patch);
-        if (json) {
-	        flagVersion.printVersionJSON();
-        } else {
-        	flagVersion.printVersion();
+        if (json)
+        {
+            flagVersion.printVersionJSON();
+        }
+        else
+        {
+            flagVersion.printVersion();
         }
     }
 
